6b64aa257b70eb630503cec02fcf9779e7168055
Loic Bertin <loiberti@z4r2p5.le-101.fr>
2018-07-04 08:44:20 +0200

    update d05

--- d05/ex06/ft_strcmp.c
+++ d05/ex06/ft_strcmp.c
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/03 18:03:22 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/03 18:12:24 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/04 08:38:44 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -21,7 +21,7 @@ int	ft_strcmp(char *s1, char *s2)
 	i = 0;
 	while (s1[i] == s2[i] && s1[i] && s2[i])
 		i++;
-	return (*s1 - *s2);
+	return (s1[i] - s2[i]);
 }
 
 int main()
--- d05/ex07/ft_strncmp.c
+++ d05/ex07/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/03 18:03:22 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/03 18:17:05 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/04 08:43:17 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
@@ -19,16 +19,18 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	unsigned int i;
 
 	i = 0;
-	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n)
+	if (!n)
+		return (0);
+	while (s1[i] == s2[i] && s1[i] && s2[i] && i < n - 1)
 		i++;
-	return (*s1 - *s2);
+	return (s1[i] - s2[i]);
 }
 
 int main()
 {
 	char a[] = "allocour";
 	char b[] = "alloci";
-	printf("%d", ft_strncmp(a, b, 4));
-	printf("%d", strncmp(a, b, 4));
+	printf("%d", ft_strncmp(a, b, 0));
+	printf("%d", strncmp(a, b, 0));
 	return (0);
 }
