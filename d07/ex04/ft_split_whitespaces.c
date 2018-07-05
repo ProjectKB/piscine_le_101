268f7051b082a2bcf3db21490c211f54657903f1
Loic Bertin <loiberti@z4r2p5.le-101.fr>
2018-07-05 18:54:35 +0200

    update d07

--- d07/ex04/ft_split_whitespaces.c
+++ d07/ex04/ft_split_whitespaces.c
@@ -6,19 +6,17 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/05 16:56:10 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/05 18:21:47 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/05 18:35:43 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
 
 int		ft_count_word(char *str)
 {
-	int i;
-	int	n;
+	int		i;
+	int		n;
 
 	i = 0;
 	n = 0;
@@ -33,10 +31,9 @@ int		ft_count_word(char *str)
 	return (n);
 }
 
-
-int ft_length_word(char *str, int p)
+int		ft_length_word(char *str, int p)
 {
-	int	c;
+	int		c;
 
 	c = 0;
 	while ((!(str[p] == ' ' || str[p] == '\n' || str[p] == '\n')) && str[p])
@@ -47,11 +44,11 @@ int ft_length_word(char *str, int p)
 	return (c);
 }
 
-void ft_fill_tab(char *str, char **tab)
+void	ft_fill_tab(char *str, char **tab)
 {
-	int	i;
-	int	j;
-	int	p;
+	int		i;
+	int		j;
+	int		p;
 
 	i = 0;
 	p = 0;
@@ -73,43 +70,11 @@ void ft_fill_tab(char *str, char **tab)
 	tab[ft_count_word(str)] = 0;
 }
 
-void ft_putchar(char c)
+char	**ft_split_whitespaces(char *str)
 {
-	write(1, &c, 1);
-}
-
-void ft_print_tables(char **tab)
-{
-	int	i;
-	int	j;
-
-	i = 0;
-	j = 0;
-	while (tab[i])
-	{
-		while (tab[i][j])
-		{
-			ft_putchar(tab[i][j]);
-			j++;
-		}
-		ft_putchar('\n');
-		j = 0;
-		i++;
-	}
-}
-
-char **ft_split_whitespaces(char *str)
-{
-	char **tab;
+	char	**tab;
 
 	tab = (char**)malloc(sizeof(**tab) * (ft_count_word(str) + 1));
 	ft_fill_tab(str, tab);
 	return (tab);
-}
-
-int main()
-{
-	char x[] = "bonjour coucou bonjour";
-	ft_print_tables(ft_split_whitespaces(x));
-	return (0);
 }
