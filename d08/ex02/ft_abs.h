f4841a13d8883760a41deceea1d4e79dc02a9862
Loic Bertin <loiberti@z4r2p5.le-101.fr>
2018-07-06 16:21:52 +0200

    add ex02

--- d08/ex02/ft_abs.h
+++ d08/ex02/ft_abs.h
@@ -1,27 +1,20 @@
 /* ************************************************************************** */
 /*                                                          LE - /            */
 /*                                                              /             */
-/*   ft_boolean.h                                     .::    .:/ .      .::   */
+/*   ft_abs.h                                         .::    .:/ .      .::   */
 /*                                                 +:+:+   +:    +:  +:+:+    */
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
-/*   Created: 2018/07/06 15:09:33 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/06 15:58:02 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Created: 2018/07/06 16:10:42 by loiberti     #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/06 16:20:17 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#ifndef FT_BOOLEAN
+#ifndef FT_ABS_H
 
-#define FT_BOOLEAN
+#define FT_ABS_H
 
-#include <unistd.h>
-#define TRUE 1
-#define FALSE 0
-#define SUCCESS 0
-#define EVEN_MSG "J'ai un nombre pair d'arguments."
-#define ODD_MSG "J'ai un nombre impair d'arguments."
-#define EVEN(x) x
-typedef int t_bool;
+#define ABS(Value) ((Value > 0) ? Value : -Value)
 
 #endif
---
+++ d08/ex02/test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "ft_abs.h"
+
+int	main()
+{
+	int a;
+
+	a = 0;
+	printf("%d", ABS(a));
+	return (0);
+}
