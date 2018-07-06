cc84c0b441db57e411ad3ae07662319c7af86361
Loic Bertin <loiberti@z4r2p5.le-101.fr>
2018-07-06 15:55:13 +0200

    add beg d08

---
+++ d08/ex01/ft_boolean.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_boolean.h                                     .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/07/06 15:09:33 by loiberti     #+#   ##    ##    #+#       */
+/*   Updated: 2018/07/06 15:54:05 by loiberti    ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef FT_BOOLEAN
+
+#define FT_BOOLEAN
+
+#include <unistd.h>
+#define TRUE 1
+#define FALSE 0
+#define SUCCESS 0
+#define EVEN_MSG "J'ai un nombre pair d'arguments."
+#define ODD_MSG "J'ai un nombre impair d'arguments."
+#define EVEN(x) x
+typedef int t_bool;
+
+#endif
