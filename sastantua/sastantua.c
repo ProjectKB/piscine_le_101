5b21b19be316070a86bad8ab2e1bff2b996f3b2c
Loic Bertin <loiberti@z4r2p4.le-101.fr>
2018-07-07 18:07:41 +0200

    add sas final

--- sastantua/sastantua.c
+++ sastantua/sastantua.c
@@ -6,22 +6,18 @@
 /*   By: loiberti <marvin@le-101.fr>                +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/07/07 17:17:19 by loiberti     #+#   ##    ##    #+#       */
-/*   Updated: 2018/07/07 17:54:50 by loiberti    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/07/07 17:21:14 by loiberti    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-void	ft_putchar(char c);
+int	g_i_count;
+int	g_j_count;
+int	g_height_handle;
+int	g_count_floor;
+int	g_l;
 
-typedef struct sast
-{
-	int	g_i_count;
-	int	g_j_count;
-	int	g_height_handle;
-	int	g_count_floor;
-	int	g_l;
-	int g_h;
-}	sastoune;
+void	ft_putchar(char c);
 
 int		ft_width_size(int i)
 {
@@ -30,21 +26,21 @@ int		ft_width_size(int i)
 	return (ft_width_size(i - 1) + 2 * (1 + (3 + i) + (i % 2 + i / 2)));
 }
 
-void	ft_print_pyramid(int w_door, int count, sastoune *sas, int size)
+void	ft_print_pyramid(int w_door, int count, int h, int size)
 {
 	int	s_door;
 	int	stock;
 
 	stock = w_door;
-	s_door = sas->g_l / 2 + (3 + size / 2);
-	while (sas->g_j_count < sas->g_l)
+	s_door = g_l / 2 + (3 + size / 2);
+	while (g_j_count < g_l)
 	{
-		if (sas->g_i_count >= sas->g_height_handle && sas->g_j_count +
-		count >= s_door && w_door != 0 && sas->g_count_floor == size - 1)
+		if (g_i_count >= g_height_handle && g_j_count + count >= s_door &&
+		w_door != 0 && g_count_floor == size - 1)
 		{
-			if (sas->g_i_count == (sas->g_h - sas->g_height_handle) / 2 +
-			sas->g_height_handle && sas->g_j_count == (sas->g_l / 2 +
-			sas->g_h - sas->g_i_count) + (stock / 2 - 1) && size > 4)
+			if (g_i_count == (h - g_height_handle) / 2 +
+			g_height_handle && g_j_count == (g_l / 2 + h - g_i_count) +
+			(stock / 2 - 1) && size > 4)
 				ft_putchar('$');
 			else
 				ft_putchar('|');
@@ -52,35 +48,35 @@ void	ft_print_pyramid(int w_door, int count, sastoune *sas, int size)
 		}
 		else
 			ft_putchar('*');
-		sas->g_j_count++;
+		g_j_count++;
 	}
 	ft_putchar('\\');
-	sas->g_i_count++;
+	g_i_count++;
 }
 
-void	ft_print_space_and_pyramid(int e, int size, sastoune *sas)
+void	ft_print_space_and_pyramid(int h, int e, int size)
 {
 	int	w_door;
 	int	count;
 	int	stock;
 
-	sas->g_i_count = 0;
+	g_i_count = 0;
 	count = 0;
 	w_door = size;
 	if (size % 2 == 0)
 		w_door = size - 1;
 	stock = w_door;
-	while (sas->g_i_count < sas->g_h)
+	while (g_i_count < h)
 	{
-		sas->g_j_count = -e;
-		while (sas->g_j_count < sas->g_h - 1 - sas->g_i_count)
+		g_j_count = -e;
+		while (g_j_count < h - 1 - g_i_count)
 		{
 			ft_putchar(' ');
-			sas->g_j_count++;
+			g_j_count++;
 		}
-		sas->g_j_count += sas->g_h + 1 - sas->g_i_count;
+		g_j_count += h + 1 - g_i_count;
 		ft_putchar('/');
-		ft_print_pyramid(w_door, count, sas, size);
+		ft_print_pyramid(w_door, count, h, size);
 		count++;
 		w_door = stock;
 		ft_putchar('\n');
@@ -90,22 +86,22 @@ void	ft_print_space_and_pyramid(int e, int size, sastoune *sas)
 void	sastantua(int size)
 {
 	int	i;
+	int	h;
 	int	e;
-	sastoune sas;
-	
+
 	if (size % 2)
-		sas.g_height_handle = 2;
+		g_height_handle = 2;
 	else
-		sas.g_height_handle = 3;
+		g_height_handle = 3;
 	i = 0;
-	sas.g_count_floor = 0;
+	g_count_floor = 0;
 	while (i < size)
 	{
 		e = (ft_width_size(size - 1) - ft_width_size(i)) / 2;
-		sas.g_h = 3 + i;
-		sas.g_l = ft_width_size(i);
-		ft_print_space_and_pyramid(e, size, &sas);
+		h = 3 + i;
+		g_l = ft_width_size(i);
+		ft_print_space_and_pyramid(h, e, size);
 		i++;
-		sas.g_count_floor++;
+		g_count_floor++;
 	}
 }
